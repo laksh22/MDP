b65651daa3e3bc8e5218e836faf2369e511e58b6
Voon <voonhousu@gmail.com>
2020-02-04 21:03:54 +0800

    [RPI-0004] Adding initial serial implementation

--- RPi/serial/serial.h
+++ RPi/serial/serial.h
@@ -1,4 +1,6 @@
 #ifndef RPI_SERIAL_H
 #define RPI_SERIAL_H
 
+extern int fd_serial;
+
 #endif //RPI_SERIAL_H
--- RPi/settings.h
+++ RPi/settings.h
@@ -3,5 +3,7 @@
 
 #define MAX 1024
 #define SA struct sockaddr
+#define SERIAL_PORT "/dev/ttyACM0"
+#define BAUD 9600
 
 #endif //RPI_RPI_SETTINGS_H_
