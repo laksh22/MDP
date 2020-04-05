23b49454f2f33614f82c439d7f366fc28cd60d20
Ming Jun Siek <siekmingjun@hotmail.com>
2020-04-05 16:35:12 +0800

    Algorithm (#81)
    * Added algo
    
    * Updated today
    
    * updated
    
    * final
    
    * update
    
    * update

---
+++ Algorithm/src/main/RobotSystem.java
@@ -0,0 +1,30 @@
+package main;
+import java.awt.EventQueue;
+
+
+import simulator.Controller;
+
+
+public class RobotSystem {
+	
+	private static boolean _isRealRun = false;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new RobotSystem();
+			}
+		});
+	}
+	
+	public RobotSystem() {
+		Controller c = Controller.getInstance();
+		c.run();
+
+	}
+	
+	public static boolean isRealRun() {
+		return _isRealRun;
+	}
+
+}
